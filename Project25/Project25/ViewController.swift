14c45b8223a8af138877458deb2a8333186a3173
Álvaro Ávalos Hernández <avalosha0805@gmail.com>
2020-10-09 21:11:50 -0500

    Setup project.

---
+++ Project25/Project25/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project25
+//
+//  Created by Álvaro Ávalos Hernández on 09/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
